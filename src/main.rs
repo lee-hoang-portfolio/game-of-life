1d3e16d1b9e64cf204c553880777f4c9e6dae890
Lee Hoang <leehoang97@gmail.com>
2026-01-26 15:29:37 -0800

    add game-of-life engine code and starter code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod life;
+use life::*;
+
 fn main() {
     println!("PLACEHOLDER");
+
+    // Define an empty board.
+    // All values are u8 as specified in life.rs.
+    let mut fb = [
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+        [0u8, 0u8, 0u8, 0u8, 0u8],
+    ];
+
+    // test that the board fb can be passed to the life function.
+    life(&mut fb);
+
+    // test that the board can be passed to the done function.
+    let is_done = done(&fb);
+    println!("{}",is_done);
+
 }
