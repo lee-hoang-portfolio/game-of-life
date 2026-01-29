3cc295e0ba2b7774fc2f99fcb7ab4fbc592e9dc1
Lee Hoang <leehoang97@gmail.com>
2026-01-28 17:08:41 -0800

    add random board generation and game of life functionality

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 // Use statements
 
 // random number generator
-// use nanorand::{pcg64::Pcg64, Rng, SeedableRng};
+use nanorand::{pcg64::Pcg64, Rng};
 
 // cortex and microbit
 use cortex_m_rt::entry;
@@ -13,6 +13,7 @@ use microbit::{
     board::{Board},
     display::blocking::Display,
     hal::{
+        Rng as HwRng,
         timer::Timer // timer
     }
 };
@@ -21,9 +22,9 @@ use microbit::{
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
-// Game of Life engine - TBD
-// mod life;
-// use life::*;
+// Game of Life engine
+mod life;
+use life::*;
 
 // =====================================================================
 
@@ -32,6 +33,7 @@ use rtt_target::{rprintln, rtt_init_print};
 fn main() -> ! {
     // 
     rtt_init_print!();
+    rprintln!("Starting Game of Life");
 
     // define the board
     let board = Board::take().unwrap();
@@ -39,27 +41,57 @@ fn main() -> ! {
     let mut display = Display::new(board.display_pins);
 
     // RNG stuff
-    // TBD
+    let mut hw_rng = HwRng::new(board.RNG);         // use the board's RNG to set up HW RNG
+    let hal_seed = hw_rng.random_u64();             // generate a random u64
+    let mut rng = Pcg64::new_seed(hal_seed.into()); // convert the random u64 into u128 and set up the new rng
 
-    // sample board - 
-    let fb = [
+    // sample board - it is currently a box
+    let mut start_board = [
         [1u8, 1u8, 1u8, 1u8, 1u8],
         [1u8, 0u8, 0u8, 0u8, 1u8],
         [1u8, 0u8, 0u8, 0u8, 1u8],
         [1u8, 0u8, 0u8, 0u8, 1u8],
         [1u8, 1u8, 1u8, 1u8, 1u8],
     ];
+    
+    // Generate a random board
+    for i in 0..5 {
+        for j in 0..5 {
+            let zero_or_one = rng.generate_range(0..=1); // choose either 0 or 1
+            start_board[i][j] = zero_or_one; // if 1, light up the LED
+        }
 
-    // for accessing Button A
+    }
+
+    // for accessing Button A - TBD
     // let left_button = board.buttons.button_a;
 
     // required loop
     // all behavior will take place inside the loop 
     loop {
         // light up the leds on the microbit
-        display.show(&mut timer, fb, 1000);
+        display.show(&mut timer, start_board, 1000);
         display.clear(); // clear the display
-        timer.delay(2000); // delay
+
+        // if all lights are off (zero), wait 5 frames for a button press and then generate a new random board
+        if done(&start_board) {
+            rprintln!("Board is all 0, resetting in 5 frames");
+            timer.delay(2000); // delay
+
+            // TBD - button press received
+
+            // TBD - no button press received - generate a random board
+            for i in 0..5 {
+                for j in 0..5 {
+                    let zero_or_one = rng.generate_range(0..=1);
+                    start_board[i][j] = zero_or_one;
+                }
+
+            }
+        }
+
+        // run the game of life
+        life(&mut start_board);
     }
 
 }
